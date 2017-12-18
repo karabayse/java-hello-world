e674aa94ce7370571a0b4f2503e1bf113d17f107
Kara Bayse <karabayse@gmail.com>
2017-12-18 15:33:53 -0600

    whitespace

--- client.java
+++ client.java
@@ -16,3 +16,12 @@ public class Variables {
 
 	}
 }
+
+
+public class WhiteSpace {
+	public static void main(String[] args) {
+
+		boolean isFormatted = false;System.out.println(isFormatted);
+		System.out.println(isFormatted);
+	}
+}
