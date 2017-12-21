93d3fd68c1223293a5be0267c2b7b13e44da9076
Kara Bayse <karabayse@gmail.com>
2017-12-21 15:48:27 -0600

    math and the modulo

--- client.java
+++ client.java
@@ -25,3 +25,13 @@ public class WhiteSpace {
 		System.out.println(isFormatted);
 	}
 }
+
+
+public class Modulo {
+	public static void main(String[] args) {
+
+		int myRemainder = 20 % 9;
+		System.out.println(myRemainder);
+
+	}
+} 
